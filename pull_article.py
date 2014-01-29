7fca655629e8444733c657d8d00e438ab161928e
ETF <ekufalo@gmail.com>
2014-01-28 21:00:10 -0800

    Function version

--- pull_article.py
+++ pull_article.py
@@ -2,10 +2,14 @@ import requests
 import nltk
 
 url = 'http://www.nytimes.com'
-source = requests.get(url)
-source = source.text
 
-clean = nltk.clean_html(source)
-tokens = nltk.word_tokenize(clean)
-freqdist = nltk.FreqDist(tokens)
-freqdist.items()
+def nltk_pulling(url):
+    source = requests.get(url)
+    source = source.text
+
+    clean = nltk.clean_html(source)
+    tokens = nltk.word_tokenize(clean)
+    freqdist = nltk.FreqDist(tokens)
+    return freqdist.items()
+
+nltk_pulling()
